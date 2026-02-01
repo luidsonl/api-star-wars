e87e7428b3e64a3bc4a1cb9202dccd5f20910f90
Luidson Lima <luidsonlsti@gmail.com>
2026-01-31 23:23:32 -0300

    update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import functions_framework
 from flask import Flask, jsonify
 from app.routes import register_routes
 
@@ -13,30 +12,8 @@ def not_found(e):
 def internal_error(e):
     return jsonify({"error": "Internal server error"}), 500
 
-@functions_framework.http
-def api_star_wars(request):
-    """
-    HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>
-    """
-    # Cloud Functions (functions-framework) handles the request context.
-    # We can use the app to dispatch the request.
-    with app.test_request_context(
-        path=request.path,
-        base_url=request.base_url,
-        query_string=request.query_string,
-        method=request.method,
-        headers=dict(request.headers),
-        data=request.get_data(),
-    ):
-        try:
-            rv = app.full_dispatch_request()
-        except Exception as e:
-            rv = app.handle_exception(e)
-        return app.make_response(rv)
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google Cloud
+    # Run, a webserver process such as Gunicorn will serve the app.
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
